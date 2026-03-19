c9eb0f85baf70ddf14042f86f97d770633c3cea6
JuhaszLaszlo69 <jlacko0926@gmail.com>
2026-03-19 20:37:26 +0100

    Added the Hozzadas function to autoKereskedes Class

--- ConsoleAppJobbMintAzExcell/ConsoleAppJobbMintAzExcell/Auto.cs
+++ ConsoleAppJobbMintAzExcell/ConsoleAppJobbMintAzExcell/Auto.cs
@@ -8,21 +8,21 @@ namespace ConsoleAppJobbMintAzExcell
 {
     internal class Auto
     {
-        public string Nev {  get; }
+        public string Nev { get; }
         public long BerlesAra { get; }
         public bool VanBerelve { get; }
         public bool VanBiztositas { get; }
-        public DateTime BiztositasKezdete { get;}
+        public DateTime BiztositasKezdete { get; }
         public DateTime KiberlesKezdete { get; }
         public DateTime KiberlesVege { get; }
-        
 
-        HashSet<string> LehetsegesAutoMarkak = {"Opel", "Toyota", "BYD", "Volkswagen", "Tesla",
+
+        HashSet<string> LehetsegesAutoMarkak = new HashSet<string>{"Opel", "Toyota", "BYD", "Volkswagen", "Tesla",
             "Honda", "BMW", "Hyundai", "Ford", "Mercedes-Benz", "Geely Group", "Kia",
             "Nissan", "Porsche", "Subaru", "General Motors", "GM", "Volvo", "Audi","Mazda",
             "Ferrari", "Suziki"};
 
-        public Auto(string Nev, long BerlesAra, bool VanBiztositas ) 
+        public Auto(string Nev, long BerlesAra, bool VanBiztositas)
         {
             this.Nev = Nev;
             this.BerlesAra = BerlesAra;
--- ConsoleAppJobbMintAzExcell/ConsoleAppJobbMintAzExcell/AutokolcsonzoRendszer.cs
+++ ConsoleAppJobbMintAzExcell/ConsoleAppJobbMintAzExcell/AutokolcsonzoRendszer.cs
@@ -11,5 +11,19 @@ namespace ConsoleAppJobbMintAzExcell
         List<Auto> autok;
 
         public List<Auto> Autok { get => new List<Auto>(autok); }
+
+        public AutokolcsonzoRendszer()
+        {
+            this.autok = new List<Auto>();
+        }
+
+        public void FelVetel(string neve, long berlesAra, bool vanbiztositasa)
+        {
+            Auto auto = new Auto(neve, berlesAra, vanbiztositasa);
+            if (berlesAra > 0)
+            {
+                autok.Add(auto);
+            }
+        }
     }
 }
