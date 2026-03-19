cfec653b754de7bc0452793b724e98e361a58de9
JuhaszLaszlo69 <jlacko0926@gmail.com>
2026-03-19 21:00:46 +0100

    Updated the add function in Autokereskedes class

--- ConsoleAppJobbMintAzExcell/ConsoleAppJobbMintAzExcell/Auto.cs
+++ ConsoleAppJobbMintAzExcell/ConsoleAppJobbMintAzExcell/Auto.cs
@@ -10,17 +10,17 @@ namespace ConsoleAppJobbMintAzExcell
     {
         public string Nev { get; }
         public long BerlesAra { get; }
-        public bool VanBerelve = false { get; }
+        public bool VanBerelve { get; }
         public DateTime KiberlesKezdete { get; }
         public DateTime KiberlesVege { get; }
-        public bool VanBiztositas = false { get; }
+        public bool VanBiztositas { get; }
 
 
         HashSet<string> LehetsegesAutoMarkak = new HashSet<string>{"Opel", "Toyota", "BYD", "Volkswagen", "Tesla",
             "Honda", "BMW", "Hyundai", "Ford", "Mercedes-Benz", "Geely Group", "Kia",
             "Nissan", "Porsche", "Subaru", "General Motors", "GM", "Volvo", "Audi","Mazda",
             "Ferrari", "Suziki"};
-        
+
         public Auto(string Nev, long BerlesAra, bool VanBerelve, DateTime KiberlesKezdete, DateTime KiberlesVege, bool VanBiztositas)
         {
             this.Nev = Nev;
