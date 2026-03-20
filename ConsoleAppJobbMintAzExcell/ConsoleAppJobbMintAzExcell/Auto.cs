fb20b6ae75243f6d85f91d70a6534c1c0281f804
Ojetimi-Puha Morgan <158039574+TheBossGoon@users.noreply.github.com>
2026-03-20 09:54:33 +0100

    Auto class kezdetleges

--- ConsoleAppJobbMintAzExcell/ConsoleAppJobbMintAzExcell/Auto.cs
+++ ConsoleAppJobbMintAzExcell/ConsoleAppJobbMintAzExcell/Auto.cs
@@ -8,5 +8,25 @@ namespace ConsoleAppJobbMintAzExcell
 {
     internal class Auto
     {
+        public string Nev {  get; }
+        public long BerlesAra { get; }
+        public bool VanBerelve { get; }
+        public bool VanBiztositas { get; }
+        public DateTime BiztositasKezdete { get;}
+        public DateTime KiberlesKezdete { get; }
+        public DateTime KiberlesVege { get; }
+        
+
+        HashSet<string> LehetsegesAutoMarkak = {"Opel", "Toyota", "BYD", "Volkswagen", "Tesla",
+            "Honda", "BMW", "Hyundai", "Ford", "Mercedes-Benz", "Geely Group", "Kia",
+            "Nissan", "Porsche", "Subaru", "General Motors", "GM", "Volvo", "Audi","Mazda",
+            "Ferrari", "Suziki"};
+
+        public Auto(string Nev, long BerlesAra, bool VanBiztositas ) 
+        {
+            this.Nev = Nev;
+            this.BerlesAra = BerlesAra;
+            this.VanBiztositas = VanBiztositas;
+        }
     }
 }
